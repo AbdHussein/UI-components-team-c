f3440c7a1dfe07569ea70136cd19c4cf630c95f8
Mohammed Almallahi <almallahi@outlook.com>
2022-01-04 17:09:53 +0200

    add the rest of clours to Theme object

--- src/components/Theme/Theme.js
+++ src/components/Theme/Theme.js
@@ -1,9 +1,39 @@
 export const lightTheme = {
-  primary: "#3f50b5",
-  secondary: "#f44336",
+  colors: {
+    primary: "#3f50b5",
+    secondary: "#f44336",
+    error: "#ef5350",
+    success: "#4caf50",
+    warning: "#ff9800",
+    info: "#03a9f4",
+  },
+  textColors: {
+    primary: "#000000DE",
+    secondary: "#00000099",
+    disabled: "#00000061",
+  },
+  background: {
+    paper: "#fff",
+    default: "#fff",
+  },
 };
 
 export const darkTheme = {
-  primary: "#002884",
-  secondary: "#ba000d",
+  colors: {
+    primary: "#002884",
+    secondary: "#ba000d",
+    error: "#c62828",
+    success: "#1b5e20",
+    warning: "#e65100",
+    info: "#01579b",
+  },
+  textColors: {
+    primary: "#000000DE",
+    secondary: "#00000099",
+    disabled: "#00000061",
+  },
+  background: {
+    paper: "#fff",
+    default: "#fff",
+  },
 };
