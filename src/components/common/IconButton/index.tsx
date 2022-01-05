d74efabf1cb8690407f413796203a077c287edaf
SHOMANS <mohammedshoman9@gmail.com>
2022-01-05 03:52:54 +0200

    create Icon button & add arrow svg component

---
+++ src/components/common/IconButton/index.tsx
@@ -0,0 +1,15 @@
+import { FC, HTMLProps, ReactNode } from "react";
+import { StyledIconButton } from "./style";
+
+interface IProps extends HTMLProps<HTMLButtonElement> {
+  children: ReactNode;
+  component?: any;
+}
+
+export const IconButton: FC<IProps> = ({ children, component, ...rest }) => {
+  return (
+    <StyledIconButton as={component} {...rest}>
+      {children}
+    </StyledIconButton>
+  );
+};
