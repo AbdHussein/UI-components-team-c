470703d46b6ee73442fe85eb723b065a11954a89
abddulrahmaanHussein <a.hussein@fis.ps>
2022-01-07 00:24:58 +0200

    Resolving conflict

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,13 @@ import React, { useState } from "react";
 import "./App.css";
 import { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme } from "./components/Theme/Theme";
-import DialogEx from "./components/Dialog/DialogEx";
-
 
 function App() {
   const [theme, setTheme] = useState("light");
 
   return (
     <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
-      <div className="App"> App
-        <DialogEx />
-      </div>
+      <div className="App"></div>
     </ThemeProvider>
   );
 }
