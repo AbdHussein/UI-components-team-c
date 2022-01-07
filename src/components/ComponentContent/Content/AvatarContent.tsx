46de2ce2f1e50d9f72210f901a1bc1c5527077ad
SHOMANS <mohammedshoman9@gmail.com>
2022-01-07 20:49:57 +0200

    create the components content layout create new components

---
+++ src/components/ComponentContent/Content/AvatarContent.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AvatarContent = () => {
+  return <div>avatar content</div>;
+};
+
+export default AvatarContent;
---
+++ src/components/ComponentContent/Content/SkeletonContent.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SkeletonContent = () => {
+  return <div>skeleton content</div>;
+};
+
+export default SkeletonContent;
