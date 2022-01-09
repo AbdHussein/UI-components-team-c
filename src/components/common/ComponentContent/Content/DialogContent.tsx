dce82b843e2ce137d1b659104c7c6af4c3b0b283
abddulrahmaanHussein <a.hussein@fis.ps>
2022-01-09 12:22:16 +0200

    Add Dialog Content

--- src/components/common/ComponentContent/Content/DialogContent.tsx
+++ src/components/common/ComponentContent/Content/DialogContent.tsx
@@ -1,13 +1,39 @@
 import React from "react";
 import Footer from "../../Footer";
 import TableOfContent from "../../../common/TableOfContent";
-import AvatarTableOfContent from "../../../common/TableOfContent/Avatar";
+import Typography from "../../../Typography";
+import ComponentOverview from "../../ComponentOverview";
+import DialogEx from "../../../Dialog/DialogExample";
 
 const DialogContent = () => {
   return (
     <>
       <div>
-        dialog content
+        <Typography variant="h1" id="Dialog">
+          Dialog
+        </Typography>
+        <br />
+        <Typography variant="body1" gutterBottom>
+          Dialogs inform users about a task and can contain critical
+          information, require decisions, or involve multiple tasks.
+        </Typography>
+        <Typography variant="h2" id="Basic_dialog">
+          Basic dialog
+        </Typography>
+        <br />
+        <Typography variant="body1" gutterBottom>
+          Simple dialogs can provide additional details or actions about a list
+          item. For example, they can display avatars, icons, clarifying
+          subtext, or orthogonal actions (such as adding an account). Touch
+          mechanics: <br />
+          - Choosing an option immediately commits the option and closes the
+          menu. <br />- Touching outside of the dialog, or pressing Back,
+          cancels the action and closes the dialog
+        </Typography>
+        <br />
+        <ComponentOverview style={{ gap: "5px" }}>
+          <DialogEx />
+        </ComponentOverview>
         <Footer />
       </div>
       <div style={{ width: "240px" }}>
